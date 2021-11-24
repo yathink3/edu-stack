822b8bf8608373e8592120bd06a33d67eddc0477
yathin <yathink3@gmail.com>
2021-11-25 00:52:08 +0530

    git pushed changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
@@ -7,5 +8,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('tailwindcss-neumorphism')],
 };
